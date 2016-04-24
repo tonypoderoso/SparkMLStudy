257642c7c9dcd8202f041d71f1543d546c2f293a
tonyp <tonypoderoso@gmail.com>
2016-04-24 21:23:25 +0900

    from LG

--- src/main/scala/algorithms/minRedundancyMaxRelevanceFeatureSelection.scala
+++ src/main/scala/algorithms/minRedundancyMaxRelevanceFeatureSelection.scala
@@ -1,6 +1,6 @@
 package algorithms
 
-import breeze.linalg.{Transpose, argmax, max, DenseMatrix => BDM, DenseVector => BDV}
+import breeze.linalg.{Transpose, argmax, max, sum, DenseMatrix => BDM, DenseVector => BDV}
 import org.apache.spark.mllib.linalg._
 import org.apache.spark.mllib.linalg.distributed.RowMatrix
 import org.apache.spark.mllib.regression.LabeledPoint
@@ -15,8 +15,12 @@ class minRedundancyMaxRelevanceFeatureSelection {
   def evaluate(H: BDM[Double], f: BDV[Double], num_feat: Int): BDV[Int] = {
     val mRMRSorting: BDV[Int] = BDV.zeros[Int](num_feat)
     val selected: BDV[Double] = BDV.zeros[Double](num_feat)
+
     mRMRSorting(0) = argmax(f.toArray)
+    printf("THe first f:  " + f.map(x=> x.toString + ","))
+    printf("the maximum argument is : " +argmax(f.toArray))
     selected(mRMRSorting(0)) = 1
+    println("The selected vector is : " + selected.map(x=>x.toString + ","))
 
     println(" Inside MRMR")
 
@@ -28,22 +32,28 @@ class minRedundancyMaxRelevanceFeatureSelection {
 
     for (kk<-1 until num_feat){
     val red: IndexedSeq[Double] = tmp.map { case (x: BDV[Double], y) =>
-      if (selected(y) == 1) {
-        0
+      println( "Iteration : "+ kk)
+      if (selected(y) == 1.0) {
+        0.0
       }else {
         //println("\nThe index : " + y)
         //println("\nthe x : " + x.map(x=> x.toString + " " ).reduce(_+_))
         //println("\nThe selected: " + selected.map(x => x.toString + " " ).reduce(_+_))
-         x.t * selected
-        //println("\nThe result: " + ret)
+        val ret = x.t * selected
+        println("\nThe result: " + ret + " y : " + y)
+        ret
       }
     }
-      val medicalc=f+BDV(red.toArray).map(_/(-kk))
-      println( "Iteration : "+ kk)
+      val newf: BDV[Double] =selected.mapPairs{ (x,y)=>if (y==1.0) 0.0 else f(x)}
+      val medicalc=newf+BDV(red.toArray).map(_/(-kk))
+      println(" The criterion : ")
       medicalc.foreach(println)
+
       mRMRSorting(kk)=argmax(medicalc)
-      selected(mRMRSorting(kk))=1
-      red.map(x=>0)
+      selected(argmax(medicalc))=1.0
+      println("THe MRMR " + mRMRSorting.map(x => x.toString + ","))
+      println("The sum is " + sum(selected))
+      //red.map(x=>0)
     }
     mRMRSorting
   }
